403a4ea3e0ec5703b7d1e4cbdf7ba665f682d503
natlopar <natalialopezariel@gmail.com>
2024-02-29 11:25:21 +0100

    photos

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,14 +1,15 @@
-// Fichero src/index.jsx (código nuevo)
 
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import App from "./components/App.jsx";
 
 import { HashRouter } from "react-router-dom";
+import { StrictMode } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <StrictMode>
   <HashRouter>
     <App />
   </HashRouter>
+  </StrictMode>
 );
