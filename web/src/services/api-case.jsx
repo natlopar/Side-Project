211b8c73e239226fd4b4f86797b28ac73715e303
unknown <natalialopezariel@gmail.com>
2024-03-31 16:17:19 +0200

    change fetch

--- web/src/services/api-case.jsx
+++ web/src/services/api-case.jsx
@@ -8,7 +8,7 @@ const getPublicCases= () => {
 }
 
 const getFilterCase = (params) => {
-  return fetch(`http://localhost:4000/case?name=${params.name}&breed=${params.breed}&clinical=${params.clinic}`)
+  return fetch(`http://vetfolio-manager.onrender.com/case?name=${params.name}&breed=${params.breed}&clinical=${params.clinic}`)
   .then(response => response.json())
   .then(data => {
      return data;
