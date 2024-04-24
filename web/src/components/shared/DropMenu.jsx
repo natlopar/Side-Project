04c9559c113829f25c23fed1bd2e7135baca3b81
unknown <natalialopezariel@gmail.com>
2024-04-24 15:52:09 +0200

    change toggle dark

--- web/src/components/shared/DropMenu.jsx
+++ web/src/components/shared/DropMenu.jsx
@@ -1,22 +1,22 @@
-import '../../styles/header.scss'
-import { Link } from 'react-router-dom';
-import Dropdown from 'react-bootstrap/Dropdown';
+// import '../../styles/header.scss'
+// import { Link } from 'react-router-dom';
+// import Dropdown from 'react-bootstrap/Dropdown';
 
 
-function DropMenu() {
-  return (
-    <Dropdown >
-      <Dropdown.Toggle id="dropdown-autoclose-true">
-        Menu
-      </Dropdown.Toggle>
+// function DropMenu() {
+//   return (
+//     <Dropdown >
+//       <Dropdown.Toggle id="dropdown-autoclose-true">
+//         Menu
+//       </Dropdown.Toggle>
 
-      <Dropdown.Menu className="header__style--menu">
-        <Dropdown.Item > <Link to={"/logIn"} className='link'>Inicia Sesión</Link> </Dropdown.Item>
-        <Dropdown.Item > <Link to={"/signIn"} className='link'>Regístrate</Link> </Dropdown.Item>
-        <Dropdown.Item > <Link to={"/publicList"} className='link'>Explora</Link> </Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  );
-}
+//       <Dropdown.Menu className="header__style--menu">
+//         <Dropdown.Item > <Link to={"/logIn"} className='link'>Inicia Sesión</Link> </Dropdown.Item>
+//         <Dropdown.Item > <Link to={"/signIn"} className='link'>Regístrate</Link> </Dropdown.Item>
+//         <Dropdown.Item > <Link to={"/publicList"} className='link'>Explora</Link> </Dropdown.Item>
+//       </Dropdown.Menu>
+//     </Dropdown>
+//   );
+// }
 
-export default DropMenu;
+// export default DropMenu;
--- web/src/components/shared/Header.jsx
+++ web/src/components/shared/Header.jsx
@@ -4,45 +4,48 @@ import { Toggle } from './Toggle';
 import { useState } from 'react';
 import logo from '../../images/logoVET.png';
 import MenuNav from './MenuNav';
-import PropTypes from "prop-types";
-
+import PropTypes from 'prop-types';
 
 function Header({ isDark, setIsDark }) {
+  const [dropMenu, setDropMenu] = useState(false);
+  const [moveMenu, setMoveMenu] = useState('');
 
-const [dropMenu, setDropMenu] = useState(false);
-const [moveMenu, setMoveMenu] = useState('');
-
-
-
-const handleMenu = (e)=> {
-  e.preventDefault();
-  setMoveMenu('dropMenu');
-  setDropMenu(!dropMenu);
-
-}
+  const handleMenu = (e) => {
+    e.preventDefault();
+    setMoveMenu('dropMenu');
+    setDropMenu(!dropMenu);
+  };
 
   return (
     <header className="header dark">
       <Link to={'/'}>
-        <img src={logo} alt="logo Vetfolio" className="header__image heartbeat" role="navigation" />
+        <img
+          src={logo}
+          alt="logo Vetfolio"
+          className="header__image heartbeat"
+          role="navigation"
+        />
       </Link>
       <div className="header__style">
-        <Toggle
-          isDark={isDark}
-          handleChange={() => setIsDark(!isDark)}
-        />
-        <button  className={`${moveMenu} header__menu`}  onClick={handleMenu} aria-label='Menu'><i className= "fa-solid fa-bars"></i></button>
-        {dropMenu ? <MenuNav /> : <div></div> }
-        {/* <DropMenu/> */}
+        <Toggle isDark={isDark} handleChange={() => setIsDark(!isDark)} />
+        <button
+          className={`${moveMenu} header__menu`}
+          onClick={handleMenu}
+          aria-label="Menu"
+        >
+          <i className= "fa-solid fa-bars"></i>
+          
+        </button>
+
+        {dropMenu ? <MenuNav /> : <div></div>}
       </div>
     </header>
-
   );
 }
 
 Header.propTypes = {
-  isDark: PropTypes.bool, 
-  setIsDark: PropTypes.func, 
-}
+  isDark: PropTypes.bool,
+  setIsDark: PropTypes.func,
+};
 
 export default Header;
