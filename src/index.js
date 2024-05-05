41f9b95efc084a42b54dfc7904d39fd3dffbd11d
unknown <natalialopezariel@gmail.com>
2024-05-05 20:15:12 +0200

    swagger getPublic done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
 const bcrypt = require('bcrypt');
-
 const jwt = require('jsonwebtoken');
+const swaggerUI = require('swagger-ui-express');
+const swaggerConfig = require('./swagger.json');
 require('dotenv').config();
 
 const server = express();
 
-
 server.use(cors());
 server.use(express.json({ limit: '25mb' }));
-
-
+server.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerConfig))
 const serverPort = process.env.PORT || 3000;
 server.listen(serverPort, () => {
   console.log(`Server listening at ${serverPort}`);
@@ -32,11 +30,9 @@ async function getConnection() {
   return connection;
 }
 
-
-
 //funciones token
 const generateToken = (payload) => {
-  const token = jwt.sign(payload, 'secreto', { expiresIn: '2h' }); 
+  const token = jwt.sign(payload, 'secreto', { expiresIn: '2h' });
   return token;
 };
 
@@ -49,7 +45,7 @@ const verifyToken = (token) => {
   }
 };
 
-//middleware : verifica si con ese token tienes permiso para hacer una funcion. 
+//middleware : verifica si con ese token tienes permiso para hacer una funcion.
 
 const authenticateToken = (req, res, next) => {
   const token = req.headers['authorization'];
@@ -69,12 +65,33 @@ const authenticateToken = (req, res, next) => {
 };
 
 
-// const passwordHash = await bcrypt.hash(body.password, saltRounds);
+//............CARGAR TODOS LOS CASOS PÚBLICOS-EXPLORA-----
+
+server.get('/getPublic', async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const sql = 'SELECT * FROM `case` WHERE public = 1';
+    const [results] = await connection.query(sql);
+    connection.end();
+    const response = {
+      success: true,
+      patients: results,
+    };
+    res.json(response);
+  } catch (error) {
+    console.error('Error al obtener datos:', error);
+    return res.status(404).json({
+      success: false,
+      message: 'Error al obtener datos',
+    });
+  }
+});
 
 //----------------ENDPOINT PARA REGISTRARSE, ENCRIPTAR PASSWORD Y OBTENER UN TOKEN---------------------
 server.post('/signin', async (req, res) => {
-  const { userName, nameVet, email, password, city, country, public } = req.body;
-  const connection= await getConnection();
+  const { userName, nameVet, email, password, city, country, public } =
+    req.body;
+  const connection = await getConnection();
   const selectUser = 'select * from vet where email = ?  or userName = ? ';
   const [resultSelect] = await connection.query(selectUser, [email, userName]);
 
@@ -85,7 +102,7 @@ server.post('/signin', async (req, res) => {
 
     jwt.sign(password, 'secreto', async (err, token) => {
       if (err) {
-        res.json({success: false, msg: 'Error'});
+        res.json({ success: false, msg: 'Error' });
       } else {
         const [resultInsertVet] = await connection.query(insertVet, [
           userName,
@@ -105,12 +122,12 @@ server.post('/signin', async (req, res) => {
           token: token,
           id: resultInsertVet.insertId,
           idVet: resultInsertVet.idVet,
-          nameVet: nameVet
+          nameVet: nameVet,
         });
       }
     });
   } else {
-    res.json({success: false, msg: 'Usuario ya registrado'})
+    res.json({ success: false, msg: 'Usuario ya registrado' });
   }
 });
 
@@ -124,8 +141,7 @@ server.post('/signin', async (req, res) => {
 //----------------ENDPOINT PARA INCIAR SESIÓN---------------------
 
 server.post('/login', async (req, res) => {
-
-  const {username, email, password} = req.body;
+  const { username, email, password } = req.body;
 
   //Buscar si el usuario existe en la bases de datos
   const selectUser = 'SELECT * FROM vet WHERE email = ?';
@@ -133,10 +149,7 @@ server.post('/login', async (req, res) => {
   const [users] = await connection.query(selectUser, [email]);
   connection.end();
   if (users.length !== 0) {
-    const isOkPass = await bcrypt.compare(
-      password,
-      users[0].hashed_password
-    );
+    const isOkPass = await bcrypt.compare(password, users[0].hashed_password);
     if (isOkPass) {
       // generar token
       const infoToken = {
@@ -149,7 +162,7 @@ server.post('/login', async (req, res) => {
         token: token,
         id: users[0].idVet,
         name: users[0].nameVet,
-        public: users[0].public
+        public: users[0].public,
       });
     } else {
       res.json({
@@ -165,27 +178,29 @@ server.post('/login', async (req, res) => {
   }
 });
 
-
 //----ENDPOINT CIERRE SESION----------------
 
-server.put('/logout', async (req, res) =>{
-  const authHeader = req.headers["authorization"];
-  jwt.sign(authHeader, "", { expiresIn: 2 } , (logout, err) => {
-     if (logout) {
-      res.json({success: true, message: 'se ha cerrado tu sesión'});
-     } else {
-      res.json({success: false, message: 'no se ha podido cerrar la sesión', err});
-     }
+server.put('/logout', async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  jwt.sign(authHeader, '', { expiresIn: 2 }, (logout, err) => {
+    if (logout) {
+      res.json({ success: true, message: 'se ha cerrado tu sesión' });
+    } else {
+      res.json({
+        success: false,
+        message: 'no se ha podido cerrar la sesión',
+        err,
+      });
+    }
   });
-})
-
+});
 
 //.----------------------cargar los casos del usuario autenticado.......................
 
 server.get('/listUser', authenticateToken, async (req, res) => {
   try {
     const idVet = req.headers['id'];
-    const numberId= parseInt(idVet);
+    const numberId = parseInt(idVet);
     let sql = 'SELECT * FROM `case` WHERE fk_Vet = ?';
     const connection = await getConnection();
     const [cases] = await connection.query(sql, [numberId]);
@@ -196,131 +211,136 @@ server.get('/listUser', authenticateToken, async (req, res) => {
     };
     res.json(response);
   } catch (error) {
-    console.error("Error al obtener datos:", error);
+    console.error('Error al obtener datos:', error);
   }
 });
 
 //.........eliminar un caso...................
 
-server.delete('/listUser', async (req,res)=> {
-try {
-  const idCase = req.query.id;
-  const connection = await getConnection();
-  const deleteSQL = 'DELETE from `case` WHERE idCase = ?';
-  const [result] = await connection.query(deleteSQL, [idCase]);
-  if (result.affectedRows > 0) {
-    res.json({
-      success: true,
-      message: 'El caso seleccionado ha sido eliminado correctamente',
-    });
-  } else {
+server.delete('/listUser', async (req, res) => {
+  try {
+    const idCase = req.query.id;
+    const connection = await getConnection();
+    const deleteSQL = 'DELETE from `case` WHERE idCase = ?';
+    const [result] = await connection.query(deleteSQL, [idCase]);
+    if (result.affectedRows > 0) {
+      res.json({
+        success: true,
+        message: 'El caso seleccionado ha sido eliminado correctamente',
+      });
+    } else {
+      res.json({
+        success: false,
+        message: 'No se ha podido eliminar el caso',
+      });
+    }
+  } catch (error) {
     res.json({
       success: false,
-      message: 'No se ha podido eliminar el caso',
+      message: error,
     });
   }
- 
-} catch(error) {
-  res.json({
-    success: false,
-    message: error,
-  });
-
-}
-
-
-})
+});
 
 //.................ENDPOINT AÑADIR UN CASO.............
 
 server.post('/newCase', async (req, res) => {
   const connection = await getConnection();
-  const {name, specie, breed, gender, birthday, clinical, exploration, tests, results, treatment, evolution, comments, public, fk_Vet} = req.body;
-  const insertCase = 'INSERT INTO `case` (`name`, specie, breed, gender, birthday, clinical, exploration, tests, results, treatment, evolution, comments, public, fk_Vet) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
-  const [resultCases] = await connection.query(insertCase, [ name, specie, breed, gender, birthday, clinical, exploration, tests, results, treatment, evolution, comments, public, fk_Vet]);
-  connection.end(); 
+  const {
+    name,
+    specie,
+    breed,
+    gender,
+    birthday,
+    clinical,
+    exploration,
+    tests,
+    results,
+    treatment,
+    evolution,
+    comments,
+    public,
+    fk_Vet,
+  } = req.body;
+  const insertCase =
+    'INSERT INTO `case` (`name`, specie, breed, gender, birthday, clinical, exploration, tests, results, treatment, evolution, comments, public, fk_Vet) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+  const [resultCases] = await connection.query(insertCase, [
+    name,
+    specie,
+    breed,
+    gender,
+    birthday,
+    clinical,
+    exploration,
+    tests,
+    results,
+    treatment,
+    evolution,
+    comments,
+    public,
+    fk_Vet,
+  ]);
+  connection.end();
   console.log(resultCases);
   res.json({
     success: true,
-    result: resultCases
-  })
-})
+    result: resultCases,
+  });
+});
 //......ENDPOINT MODIFICAR UN CASO.................
 
 server.patch('/updateCase/:id', async (req, res) => {
   try {
-   const connection = await getConnection();
-   const id = req.params.id;
-   const data = req.body;
- 
-   // Consulta para obtener los valores actuales del registro
-   const selectQuery = 'SELECT * FROM `case` WHERE idCase = ?';
-   const [rows] = await connection.query(selectQuery, [id]);
- 
-   if (rows.length === 0) {
-     return res.json({
-       success: false,
-       message: 'No se encontró el caso con el ID proporcionado.'
-     });
-   }
- 
-   // Para cada campo recibido en la solicitud PATCH
-   for (const key in data) {
-     if (data.hasOwnProperty(key)) {
-       // Si el campo tiene un valor no vacío, actualizarlo
-       if (data[key] !== '') {
-         rows[0][key] = data[key];
-       }
-     }
-   }
- 
-   // Construir la consulta de actualización con los campos actualizados
-   const updateFields = Object.keys(rows[0]).map(key => `${key} = ?`).join(', ');
-   const updateValues = Object.values(rows[0]);
-   updateValues.push(id);
- 
-   const updateQuery = `UPDATE \`case\` SET ${updateFields} WHERE idCase = ?`;
- 
-   // Ejecutar la consulta de actualización
-   const [result] = await connection.query(updateQuery, updateValues);
-   connection.end(); 
-   res.json({
-     success: true,
-     message: 'Actualizado correctamente',
-     casesChanged: result.affectedRows,
-   });
-  } catch (error) {
-   res.json ({
-     success: false,
-     error: error
-   });
-  }
- });
- 
- 
-
-
-//............CARGAR TODOS LOS CASOS PÚBLICOS-EXPLORA-----
-
-server.get('/getPublic', async (req, res) => {
-  try{
     const connection = await getConnection();
-    const sql = 'SELECT * FROM `case` WHERE public = 1';
-    const [results] = await connection.query(sql);
+    const id = req.params.id;
+    const data = req.body;
+
+    // Consulta para obtener los valores actuales del registro
+    const selectQuery = 'SELECT * FROM `case` WHERE idCase = ?';
+    const [rows] = await connection.query(selectQuery, [id]);
+
+    if (rows.length === 0) {
+      return res.json({
+        success: false,
+        message: 'No se encontró el caso con el ID proporcionado.',
+      });
+    }
+
+    // Para cada campo recibido en la solicitud PATCH
+    for (const key in data) {
+      if (data.hasOwnProperty(key)) {
+        // Si el campo tiene un valor no vacío, actualizarlo
+        if (data[key] !== '') {
+          rows[0][key] = data[key];
+        }
+      }
+    }
+
+    // Construir la consulta de actualización con los campos actualizados
+    const updateFields = Object.keys(rows[0])
+      .map((key) => `${key} = ?`)
+      .join(', ');
+    const updateValues = Object.values(rows[0]);
+    updateValues.push(id);
+
+    const updateQuery = `UPDATE \`case\` SET ${updateFields} WHERE idCase = ?`;
+
+    // Ejecutar la consulta de actualización
+    const [result] = await connection.query(updateQuery, updateValues);
     connection.end();
-    const response = {
+    res.json({
       success: true,
-      patients: results,
-    };
-    res.json(response);
+      message: 'Actualizado correctamente',
+      casesChanged: result.affectedRows,
+    });
   } catch (error) {
-    console.error("Error al obtener datos:", error);
-    return res.status(404).json({
-      success: false, 
-      message: 'Error al obtener datos'
+    res.json({
+      success: false,
+      error: error,
     });
   }
 });
+
 
 //................FILTROS...............
 
@@ -335,15 +355,15 @@ server.get('/case', authenticateToken, async (req, res) => {
     const values = [numberId];
 
     if (name) {
-      sql += " AND name = ?";
+      sql += ' AND name = ?';
       values.push(name);
     }
     if (breed) {
-      sql += " AND breed LIKE ?";
+      sql += ' AND breed LIKE ?';
       values.push(`%${breed}%`);
     }
     if (clinical) {
-      sql += " AND clinical LIKE ?";
+      sql += ' AND clinical LIKE ?';
       values.push(`%${clinical}%`);
     }
 
@@ -353,42 +373,39 @@ server.get('/case', authenticateToken, async (req, res) => {
       return res.status(200).json({
         success: true,
         message: 'Ningún caso con esos criterios de búsqueda',
-        patients: resultQuery
+        patients: resultQuery,
       });
     } else {
       res.status(200).json({ success: true, patients: resultQuery });
     }
     connection.end();
   } catch (error) {
-    console.error("Error al obtener datos:", error);
+    console.error('Error al obtener datos:', error);
     return res.status(404).json({
-      success: false, 
-      message: 'Error al obtener datos'
+      success: false,
+      message: 'Error al obtener datos',
     });
   }
 });
 
 //...CONTACTO....
 
-
 server.post('/contact', async (req, res) => {
-  try{
-    const {name, comments} = req.body;
-    const connection= await getConnection();
-    const insert='INSERT INTO comments (name, comments) VALUES (?,?)';
+  try {
+    const { name, comments } = req.body;
+    const connection = await getConnection();
+    const insert = 'INSERT INTO comments (name, comments) VALUES (?,?)';
     const [result] = await connection.query(insert, [name, comments]);
     console.log(result);
-   
-    res.json({success: true, message: 'mensaje enviado'})
+
+    res.json({ success: true, message: 'mensaje enviado' });
     connection.end();
   } catch (error) {
-    res.json({success:false, message: 'el mensaje no se pudo enviar'})
+    res.json({ success: false, message: 'el mensaje no se pudo enviar' });
   }
+});
 
-  });
+//....servidor Estáticos.........
 
-
-  //....servidor Estáticos.........
-
-  const staticServer = './src/public-react';
-  server.use(express.static(staticServer));
+const staticServer = './src/public-react';
+server.use(express.static(staticServer));
